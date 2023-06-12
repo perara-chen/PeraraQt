876f163a94b1ccfb7258062791a8e4411db1496b
Win Min Oo <perara.chen@yahoo.com>
2023-06-13 02:16:54 +0630

    Refactored the code base
    Refactored the code for better understanding removed unnecessary lines

--- hello_qt.py
+++ hello_qt.py
@@ -1,36 +1,35 @@
 import sys
 
 from PySide6.QtCore import Qt, QSize
+from PySide6.QtGui import QFont
 from PySide6.QtWidgets import QApplication, QLabel, QMainWindow
 
 
 class MainWindow(QMainWindow):
     """The main window of the application."""
-
+    
     def __init__(self):
-        """Initialize the main window."""
         super().__init__()
-
-        # Window settings
+        self.initialize_ui()
+    
+    def initialize_ui(self):
+        """Set up the GUI."""
         self.setWindowTitle("Perara's Qt App")
         self.setFixedSize(QSize(450, 400))
+        
+        self.setup_widget()
+        self.show()
+    
+    def setup_widget(self):
+        """Set up the widgets of the main window."""
+        hello_label = QLabel("Hello, PySide6!", self)
+        hello_label.setFont(QFont("Menlo", 28))
+        hello_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        
+        self.setCentralWidget(hello_label)
 
-        # Label widget
-        label = QLabel("Hello, PyQt!")
 
-        # Label widget's settings
-        label_font = label.font()
-        label_font.setPointSize(40)
-        label.setFont(label_font)
-        label.setAlignment(Qt.AlignCenter)
-
-        # Central widget of the window
-        self.setCentralWidget(label)
-
-
-app = QApplication(sys.argv)
-
-window = MainWindow()
-window.show()
-
-app.exec()
+if __name__ == '__main__':
+    app = QApplication([])
+    window = MainWindow()
+    sys.exit(app.exec())
