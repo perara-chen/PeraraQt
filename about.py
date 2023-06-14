726c1e3268437919923168eadbcc3bbb04c96193
Win Min Oo <perara.chen@yahoo.com>
2023-06-14 19:19:35 +0630

    Implemented a close button and added more labels
    Implemented close button and added more text labels. More Info button is also created but it is still left to implement.

--- about.py
+++ about.py
@@ -4,11 +4,11 @@ import sys
 
 from PySide6.QtCore import Qt, QSize
 from PySide6.QtGui import QFont, QPixmap
-from PySide6.QtWidgets import (QApplication, QMainWindow, QLabel, QPushButton,
-                               QMessageBox)
+from PySide6.QtWidgets import (QApplication, QLabel, QMessageBox, QPushButton,
+                               QWidget)
 
 
-class MainWindow(QMainWindow):
+class MainWindow(QWidget):
     """The main window class of the application."""
     
     def __init__(self):
@@ -18,7 +18,7 @@ class MainWindow(QMainWindow):
     def initialize_ui(self):
         """Set up the GUI."""
         self.setWindowTitle("About Me")
-        self.setFixedSize(QSize(450, 300))
+        self.setFixedSize(QSize(450, 260))
         
         self.setup_widget()
         self.show()
@@ -39,7 +39,7 @@ class MainWindow(QMainWindow):
                                 <h1>File not found</h1><p>Error: {error}</p>
                                 """, QMessageBox.StandardButton.Ok,
                                 QMessageBox.StandardButton.Ok)
-            
+        
         name_label = QLabel("Perara", self)
         name_label.setFont(QFont("Arial", 20))
         name_label.resize(110, 23)
@@ -62,7 +62,43 @@ class MainWindow(QMainWindow):
         language_list_label.resize(250, 15)
         language_list_label.move(100, 120)
         
+        contact_label = QLabel("Contact", self)
+        contact_label.setFont(QFont("Arial", 14))
+        contact_label.resize(70, 17)
+        contact_label.move(100, 155)
         
+        phone_num_label = QLabel("Phone number: +959XXXXXXXXX", self)
+        phone_num_label.setFont(QFont("Arial", 12))
+        phone_num_label.resize(200, 15)
+        phone_num_label.move(100, 180)
+        
+        close_button = QPushButton("Close", self)
+        close_button.clicked.connect(self.close_application)
+        close_button.move(235, 220)
+        
+        more_info_button = QPushButton("More Information", self)
+        more_info_button.clicked.connect(self.more_info)
+        more_info_button.move(305, 220)
+    
+    def close_application(self):
+        """Close the application."""
+        self.close()
+    
+    def more_info(self):
+        """Set up the More Info window."""
+
+
+class MoreInfo(QWidget):
+    """The More Info window class of the application."""
+    
+    def __init__(self):
+        super().__init__()
+        self.initialize_ui()
+    
+    def initialize_ui(self):
+        """Set up the GUI."""
+
+
 if __name__ == '__main__':
     app = QApplication([])
     window = MainWindow()
