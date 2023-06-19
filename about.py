d2dba9a7d01c16b857da91fd3646657623addfe8
Win Min Oo <perara.chen@yahoo.com>
2023-06-20 00:52:41 +0630

    Combined all labels to summary label and more info button is fully functional
    Combined all labels from main window to form summary label to make code clear. Replaced previous text with placeholder text. And more info button is now fully functional. A new more info dialog is implemented and will pop up when more info button is clicked.

--- about.py
+++ about.py
@@ -2,9 +2,9 @@
 # Simple About Me profile application.
 import sys
 
-from PySide6.QtCore import Qt, QSize
-from PySide6.QtGui import QFont, QPixmap
-from PySide6.QtWidgets import (QApplication, QLabel, QMessageBox, QPushButton,
+from PySide6.QtCore import QSize
+from PySide6.QtGui import QPixmap
+from PySide6.QtWidgets import (QApplication, QDialog, QLabel, QPushButton,
                                QWidget)
 
 
@@ -18,12 +18,12 @@ class MainWindow(QWidget):
     def initialize_ui(self):
         """Set up the GUI."""
         self.setWindowTitle("About Me")
-        self.setFixedSize(QSize(450, 260))
+        self.setFixedSize(QSize(450, 300))
         
-        self.setup_widget()
+        self.setup_widgets()
         self.show()
     
-    def setup_widget(self):
+    def setup_widgets(self):
         """Set up the widgets of the window."""
         profile_image = 'images/profile.svg'
         
@@ -40,45 +40,23 @@ class MainWindow(QWidget):
                                 """, QMessageBox.StandardButton.Ok,
                                 QMessageBox.StandardButton.Ok)
         
-        name_label = QLabel("Perara", self)
-        name_label.setFont(QFont("Arial", 20))
-        name_label.resize(110, 23)
-        name_label.move(100, 20)
-        
-        summary_label = QLabel("A junior Linux System Administrator and a "
-                               "person who would\nlike to be a system "
-                               "programmer.", self)
-        summary_label.setFont(QFont("Arial", 12))
-        summary_label.resize(325, 30)
-        summary_label.move(100, 50)
-        
-        language_label = QLabel("Programming Languages", self)
-        language_label.setFont(QFont("Arial", 14))
-        language_label.resize(180, 17)
-        language_label.move(100, 95)
-        
-        language_list_label = QLabel("Bash, C, Python, Swift", self)
-        language_list_label.setFont(QFont("Arial", 12))
-        language_list_label.resize(250, 15)
-        language_list_label.move(100, 120)
-        
-        contact_label = QLabel("Contact", self)
-        contact_label.setFont(QFont("Arial", 14))
-        contact_label.resize(70, 17)
-        contact_label.move(100, 155)
-        
-        phone_num_label = QLabel("Phone number: +959XXXXXXXXX", self)
-        phone_num_label.setFont(QFont("Arial", 12))
-        phone_num_label.resize(200, 15)
-        phone_num_label.move(100, 180)
+        summary_label = QLabel(
+            """<h1>Perara</h1><p>Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit.</p><h2>Programming Languages</h2><p>Bash, C,
+            Python, Swift</p><h2>Contact Information</h2><p>Phone :
+            +959XXXXXXXXX<br>Email : username@example.com</p>""",
+            self)
+        # summary_label.setFont(QFont("Arial", 12))
+        summary_label.resize(330, 230)
+        summary_label.move(100, 10)
         
         close_button = QPushButton("Close", self)
         close_button.clicked.connect(self.close_application)
-        close_button.move(235, 220)
+        close_button.move(235, 260)
         
         more_info_button = QPushButton("More Information", self)
         more_info_button.clicked.connect(self.more_info)
-        more_info_button.move(305, 220)
+        more_info_button.move(305, 260)
     
     def close_application(self):
         """Close the application."""
@@ -86,17 +64,50 @@ class MainWindow(QWidget):
     
     def more_info(self):
         """Set up the More Info window."""
+        self.more_info_window = MoreInfo()
+        self.more_info_window.show()
 
 
-class MoreInfo(QWidget):
+class MoreInfo(QDialog):
     """The More Info window class of the application."""
     
     def __init__(self):
         super().__init__()
+        self.setModal(True)
         self.initialize_ui()
     
     def initialize_ui(self):
         """Set up the GUI."""
+        self.setFixedSize(500, 310)
+        self.setup_widgets()
+    
+    def setup_widgets(self):
+        """Set up the widgets for the window."""
+        more_info_label = QLabel(
+            """<h1>More About Me</h1><p>Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit. Sed euismod<br>scelerisque tortor,
+            vitae finibus massa consequat non. Fusce iaculis odio a<br>risus
+            consectetur congue.</p><p>In hac habitasse platea dictumst.
+            Praesent tincidunt condimentum tortor, sit<br>amet eleifend enim
+            venenatis a. Nullam accumsan, odio non tincidunt<br>bibendum,
+            nisi mauris vestibulum arcu, a tincidunt massa urna sed
+            nunc.</p><p>Donec sagittis ultrices elit, at bibendum dolor. Ut
+            vel varius felis. Integer<br>convallis lacinia tellus,
+            eget vestibulum massa vulputate eu. Suspendisse<br>auctor tellus
+            sit amet lacus tincidunt, vitae semper nisi rhoncus. Nulla
+            dapibus<br>ultrices nunc, id venenatis nibh tempus a. Integer
+            posuere tellus in feugiat<br>iaculis.</p>""",
+            self)
+        more_info_label.resize(465, 240)
+        more_info_label.move(20, 15)
+        
+        ok_button = QPushButton("Ok", self)
+        ok_button.move(440, 270)
+        ok_button.clicked.connect(self.close_window)
+    
+    def close_window(self):
+        """Close the more info window."""
+        self.close()
 
 
 if __name__ == '__main__':
